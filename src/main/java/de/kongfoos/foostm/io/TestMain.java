c9ce315b891bfbc5ae9c55d4cb72818446e1cb50
Ferdinand Pyttel <ferdinand@pyttel.de>
2016-07-28 17:39:41 +0200

    add example

--- src/main/java/de/kongfoos/foostm/io/TestMain.java
+++ src/main/java/de/kongfoos/foostm/io/TestMain.java
@@ -1,12 +1,19 @@
 package de.kongfoos.foostm.io;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import de.kongfoos.foostm.model.player.PlayerImpl;
+import de.kongfoos.foostm.model.player.IPlayer;
+import de.kongfoos.foostm.model.team.ITeam;
+import de.kongfoos.foostm.view.fx.model.player.FXPlayer;
+import de.kongfoos.foostm.view.fx.model.player.FXPlayerBuilderFactory;
+import de.kongfoos.foostm.view.fx.model.team.FXTeam;
+import de.kongfoos.foostm.view.fx.model.team.FXTeamBuilderFactory;
 
 @SpringBootApplication
 public class TestMain {
@@ -17,14 +24,35 @@ public class TestMain {
 		DbManager dbManager = context.getBean(DbManager.class);
 		
 		dbManager.openDatabase("playerTestDb", new String[]{
-				PlayerImpl.class.getPackage().getName()
-//				TeamImpl.class.getPackage().getName()
+				IPlayer.class.getPackage().getName(),
+				ITeam.class.getPackage().getName()
 				});
 	    
 		EntityManagerFactory emf = dbManager.getEntityManagerFactory("playerTestDb");
-		emf.createEntityManager();	
+		EntityManager em = emf.createEntityManager();	
 		
-		dbManager.startWebServer("playerTestDb");
+		// start debug server
+		new Thread(){
+			public void run() {
+				dbManager.startWebServer("playerTestDb");
+			}
+		}.start();
+		
+		// save some data
+		em.getTransaction().begin();
+		Session session = em.unwrap(Session.class);
+		
+		FXPlayer p1 = FXPlayerBuilderFactory.create("Ferdinand", "Pyttel").build();
+		FXPlayer p2 = FXPlayerBuilderFactory.create("Patrick", "Bogdan").build();
+
+		session.save(IPlayer.class.getCanonicalName(), p1);
+		session.save(IPlayer.class.getCanonicalName(), p2);
+
+		FXTeam t = FXTeamBuilderFactory.buildDoubles(p1, p2);
+		
+		session.save(ITeam.class.getCanonicalName(), t);
+		
+		em.getTransaction().commit();
 		
 	}
 	
